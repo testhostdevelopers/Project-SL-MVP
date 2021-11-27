d26b6e05138d676ee0dbe011e3be98bdfcb6c6a3
vishal jariwala <vishalj.kamaldhari@gmail.com>
2021-11-27 09:50:00 +0530

    remove horizontal scroll

--- src/Pages/TokenPage/index.js
+++ src/Pages/TokenPage/index.js
@@ -18,7 +18,7 @@ const index = () => {
           <TheStarlightDAOSec />
           <ClimeworksSec />
           <WeAreHereSec />
-          <CreateCollectibleMultiplePopup />
+          {/* <CreateCollectibleMultiplePopup /> */}
         </>
     )
 }
